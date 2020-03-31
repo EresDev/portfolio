f51e75871d9a5234380ee0708ff6356ae990a4de
EresDev <hi@eresdev.com>
2020-03-31 15:37:51 -0700

    Working 1st version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from "./components/sidebar";
+import Hero from "./components/hero";
+import Contact from "./components/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="colorlib-page">
+          <div className='container-wrap'>
+            <Sidebar />
+            <div id='colorlib-main'>
+                <Hero />
+                <Contact />
+            </div>
+          </div>
+      </div>
   );
 }
 
